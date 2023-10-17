27cf507bcff8990b80d551d6ba6734ff5b992634
samuel21695 <samuel21695@gmail.com>
2023-10-17 14:49:03 +0900

    add changing background image when count increase

--- js/index.js
+++ js/index.js
@@ -14,6 +14,11 @@ const countText = document.getElementsByClassName('count')[0];
 
 // addEventListener when click
 clickEgg.addEventListener('click', () => {
-  
   countText.textContent = count++;
-})
+  // change background image when count increase
+  if (count > 9) {
+  clickEgg.style.background = 'url("/img/egg2.png")';
+  clickEgg.style.backgroundPosition = 'center';
+  clickEgg.style.backgroundSize = 'cover';
+  }
+});
