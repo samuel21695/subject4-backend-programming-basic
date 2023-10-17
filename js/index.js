e19c7573dff6c7b71bfb8829d2d650e67c37ef88
samuel21695 <samuel21695@gmail.com>
2023-10-17 14:40:04 +0900

    bring egg images

--- js/index.js
+++ js/index.js
@@ -8,14 +8,14 @@ const vibration = (target) => {
 }
 
 // const by class
-count = 0;
+let count = 0;
 const clickEgg = document.getElementsByClassName('egg')[0];
 const countText = document.getElementsByClassName('count')[0];
 
 // addEventListener when click
 clickEgg.addEventListener('click', () => {
-  count++
-  countText.textContent = count;
+  
+  countText.textContent = count++;
   // color change randomly
   clickEgg.style.background = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
 })
